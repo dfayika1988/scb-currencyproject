ba0d3df81356f527d571098819819254a51cdd68
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2021-04-19 00:36:58 +0100

    Create product.cpp
    add product class

---
+++ product.cpp
@@ -0,0 +1,12 @@
+enum class Currency { USD, EUR, GBP }; // the list goes further
+
+class Product
+{
+public:
+  // code omitted for brevity
+  double convert_price(Currency c) {
+    // convert to proper value
+  }
+
+  // code omitted for brevity
+};
