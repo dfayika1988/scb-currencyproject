e5de392ce1e3fd1d155aaf7681ab41224413f707
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2021-04-26 15:33:51 +0100

    Update currency.cpp
    currency cpp

--- src/currency.cpp
+++ src/currency.cpp
@@ -1,1 +1,25 @@
 
+#include <ql/currency.hpp>
+#include <utility>
+
+namespace QuantLib {
+
+    std::ostream& operator<<(std::ostream& out, const Currency& c) {
+        if (!c.empty())
+            return out << c.code();
+        else
+            return out << "null currency";
+    }
+
+    Currency::Data::Data(std::string name,
+                         std::string code,
+                         Integer numericCode,
+                         std::string symbol,
+                         std::string fractionSymbol,
+                         Integer fractionsPerUnit,
+                         const Rounding& rounding,
+                         std::string formatString,
+                         fractionsPerUnit(fractionsPerUnit),
+      rounding(rounding), triangulated(std::move(triangulationCurrency)),
+      formatString(std::move(formatString)) {}
+}
